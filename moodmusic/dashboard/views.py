3baf2e1f1c6ef77376b419df29e6d90ab7b76078
Nina Di Cara <ninadicara@gmail.com>
2020-05-20 22:33:59 +0100

    Add login_required decorator to dashboard views

--- moodmusic/dashboard/views.py
+++ moodmusic/dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html', {})
 
+@login_required
 def thanks(request):
     return render(request, 'thanks.html', {})
