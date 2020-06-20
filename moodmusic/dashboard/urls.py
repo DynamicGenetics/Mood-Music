9d6a4c7aacc1e37747298b8b2df607983d554ae7
Nina Di Cara <ninadicara@gmail.com>
2020-06-20 15:48:33 +0100

    Add new dashboard pages to URLS

--- moodmusic/dashboard/urls.py
+++ moodmusic/dashboard/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from dashboard import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('thanks', views.thanks, name='thanks')
+    path("", view=views.dashboard, name="dashboard"),
+    path("thanks", view=views.thanks, name="thanks"),
+    path("spotify-connect", view=views.spotify, name="spotify"),
+    path("mobile-surveys", view=views.mobile_guide, name="mobile_guide"),
+    path("data-drop", views.datadrop, name="datadrop"),
 ]
