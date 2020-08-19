3cb21de88f88a0eea9436f6bb58161f73cb0211f
Nina Di Cara <ninadicara@gmail.com>
2020-08-19 11:12:12 +0100

    Delete unneeded files from ema module

--- moodmusic/ema/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import EMAQuestion
-from django.forms import ModelForm
-
-
-class EMAQuestionForm(ModelForm):
-
-    class Meta:
-        model = EMAQuestion
-        fields = ['happiness', 'energy']
