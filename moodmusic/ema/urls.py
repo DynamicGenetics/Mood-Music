2f5624c4abc3f79509e0ebc3d27b1318259cf558
Nina Di Cara <ninadicara@gmail.com>
2020-08-22 19:43:56 +0100

    Added startsession url

--- moodmusic/ema/urls.py
+++ moodmusic/ema/urls.py
@@ -3,4 +3,5 @@ from .views import views
 
 urlpatterns = [
     path("", views.respond_to_incoming_message, name="ema"),
+    path("startsession", views.start_survey_session, name="startsession"),
 ]
