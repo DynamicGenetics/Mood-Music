3cccd563f8cfb8adf9b2acf7765044e786dedbb2
Nina Di Cara <ninadicara@gmail.com>
2020-08-22 10:58:31 +0100

    Update compatability with tests

--- moodmusic/ema/services.py
+++ moodmusic/ema/services.py
@@ -1,20 +1,19 @@
 """
 Module to that provides utility and routing functions for EMA survey sms messaging.
 """
-
-import PhoneNumber
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 from datetime import datetime, timedelta
 
-from .models import EMASession
+from .models import EMASession, EMAResponse, SessionState
 
 
 AUTO_MESSAGE = {
-    no_active_session: """Sorry, the last survey period has now expired.
+    "no_active_session": """Sorry, the last survey period has now expired.
     We'll message you the next time we have questions for you to complete.""",
-    message_invalid: """Sorry, we can only recieve messages that contains a number
+    "message_invalid": """Sorry, we can only recieve messages that contains a number
     from 0 to 10 with no punctuation. Please send your response again. """,
-    thanks: """Thank you! You have completed the survey."""
+    "thanks": """Thank you! You have completed the survey.""",
 }
 
 
@@ -43,17 +42,21 @@ def manage_response(number: str, text: str, recieved: datetime) -> str:
     now = timezone.now()
     one_hour_ago = now - timedelta(minutes=60)
     try:
-        session = EMASession.objects.filter(start_time__range=[now, one_hour_ago]).latest('start_time')
+        session = EMASession.objects.filter(
+            start_time__range=[now, one_hour_ago]
+        ).latest("start_time")
         state = SessionState.objects.filter(user=user, session=session)
     except EMASession.DoesNotExist:
-        return AUTO_MESSAGE['no_active_session']
+        return AUTO_MESSAGE["no_active_session"]
 
     # Assuming the above was successful, save the response and get the next message unless invalid
+    # Strip any spaces or punctuation from the message and then check if it is valid
+    text = "".join(e for e in text if e.isalnum())
     if is_valid(text):
-        save_response(state)
+        save_response(text, state)
         return next_message(state)
     else:
-        return AUTO_MESSAGE['message_invalid']
+        return AUTO_MESSAGE["message_invalid"]
 
 
 def is_valid(text: str):
@@ -69,10 +72,9 @@ def is_valid(text: str):
     bool
         True if message is valid, False otherwise
     """
-    message = text.strip()
     accepted_responses = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
-    if message in accepted_responses:
+    if text in accepted_responses:
         return True
     else:
         return False
@@ -81,39 +83,39 @@ def is_valid(text: str):
 def save_response(text: str, state: SessionState):
 
     # Save response
-    new_emaresponse = EMAResponse.create(
+    EMAResponse.create(
         session_no=state.session,
         question=state.last_question_sent,
         user=state.user,
-        response=int((text.strip()),
+        response=int(text),
     )
 
     # Update questions answered in the user's session state
-    state.questions_answered =+ 1
+    # state.questions_answered = state.questions_answered + 1
 
 
 def next_message(state: SessionState):
-    """Given the state of a 
+    """Given the state of a
 
     Parameters
     ----------
     state : SessionState
         [description]
     """
+    question = "hi again"
+
+    return question
 
 
+#     # If they have answered as many questions as are in the database...
+#     if state.messages_sent > EMAQuestions.objects.count()
 
-    # If they have answered as many questions as are in the database...
-    if state.messages_sent > EMAQuestions.objects.count()
+#     if (state.questions_answered == EMAQuestions.objects.count()) :
 
-    if (state.questions_answered == EMAQuestions.objects.count()) :
-        
-    elif state.questions_answered  .count():
+#     elif state.questions_answered  .count():
 
-    #If the state.questions answered == number of quesiotns in EMAQuestions then send 'thank you'
-        # If the state.messages_sent
-    # Get the questions that have been answered in this session from EMAResponses
-    # Get the available questions in EMAQuestions
-    # Randomly pick a question from the remaining questions
-
-    
+# If the state.questions answered == number of quesiotns in EMAQuestions then send 'thank you'
+# If the state.messages_sent
+# Get the questions that have been answered in this session from EMAResponses
+# Get the available questions in EMAQuestions
+# Randomly pick a question from the remaining questions
