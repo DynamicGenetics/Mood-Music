027b07447b5130c9e2039e0f40bc3b713e84b89a
Nina Di Cara <ninadicara@gmail.com>
2020-10-13 17:15:52 +0100

    Fix broken links to dashboard namespace

--- moodmusic/dashboard/urls.py
+++ moodmusic/dashboard/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = "dashboard"
 urlpatterns = [
-    path("", view=views.dashboard, name="dashboard"),
-    path("thanks", view=views.thanks, name="thanks"),
-    path("spotify-connect", view=views.spotify, name="spotify"),
-    path("mobile-surveys", view=views.mobile_guide, name="mobile_guide"),
-    path("data-drop", views.datadrop, name="datadrop"),
-    path("consent", views.ConsentView.as_view(), name="consent"),
+    path("", view=views.dashboard, name="index"),
+    path("thanks/", view=views.thanks, name="thanks"),
+    path("spotify-connect/", view=views.spotify, name="spotify"),
+    path("mobile-surveys/", view=views.mobile_guide, name="mobile_guide"),
+    path("data-drop/", views.datadrop, name="datadrop"),
+    path("consent/", views.ConsentView.as_view(), name="consent"),
 ]
