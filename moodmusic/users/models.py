80f52f61c50904670c36ce4bb8cfd14c7e451c6a
Nina Di Cara <ninadicara@gmail.com>
2020-10-13 11:52:57 +0100

    Remove username Add phonenumber and email

--- moodmusic/users/models.py
+++ moodmusic/users/models.py
@@ -1,17 +1,30 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import BooleanField, EmailField, CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
+from .managers import CustomUserManager
+
 
 class User(AbstractUser):
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
+    # Remove the username field
+    username = None
+
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    # ND added phonenumber
-    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    # Add email as the replacement unique id field
+    email = EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = "email"
+
+    # Add other required flags
+    phone_number = PhoneNumberField()
+    consent_granted = BooleanField(default=False)
+    phone_verified = BooleanField(default=False)
+
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
+        return reverse("users:detail", kwargs={"username": self.email})
