8a47510e7f4b09706f587e876dabd1c2848fc09e
Nina Di Cara <ninadicara@gmail.com>
2020-10-13 11:55:03 +0100

    Create custom sign up form for phone number and email

--- moodmusic/users/forms.py
+++ moodmusic/users/forms.py
@@ -1,30 +1,30 @@
-from django.contrib.auth import forms, get_user_model
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+from allauth.account.forms import SignupForm
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
+
 
 User = get_user_model()
 
 
-class UserChangeForm(forms.UserChangeForm):
-    class Meta(forms.UserChangeForm.Meta):
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
         model = User
 
 
-class UserCreationForm(forms.UserCreationForm):
-
-    error_message = forms.UserCreationForm.error_messages.update(
-        {"duplicate_username": _("This username has already been taken.")}
+class CustomSignupForm(SignupForm):
+    name = forms.CharField(max_length=30, label="Full Name")
+    phone_number = PhoneNumberField(
+        widget=PhoneNumberPrefixWidget, label=_("Phone number"), required=True,
     )
 
-    class Meta(forms.UserCreationForm.Meta):
-        model = User
-
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-
-        raise ValidationError(self.error_messages["duplicate_username"])
+    def signup(self, request, user):
+        user.name = self.cleaned_data["name"]
+        user.phone_number = self.cleaned_data["phone_number"]
+        user.email = self.clean_email()
+        user.save()
+        return user
