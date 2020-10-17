6297b31d05b7998fe439b7e6c76ab4b4b7d7614f
Nina Di Cara <ninadicara@gmail.com>
2020-10-17 23:34:41 +0100

    Fix conditions on datadrop files and add messages

--- moodmusic/dashboard/forms.py
+++ moodmusic/dashboard/forms.py
@@ -1,7 +1,5 @@
-import json
 from django import forms
 from django.contrib.auth import get_user_model
-from django.core.validators import FileExtensionValidator
 from django.core.exceptions import ValidationError
 
 
@@ -16,30 +14,8 @@ class TokenForm(forms.Form):
     token = forms.CharField(label="Verification Token")
 
 
-def validate_json(file):
-    try:
-        history = json.load(file)
-        for song in history:
-            keys = ["endTime", "artistName", "trackName", "msPlayed"]
-            for key in keys:
-                if key in song is False:
-                    raise ValidationError(
-                        "Sorry, this file does not contain the right information."
-                    )
-    except UnicodeDecodeError:
-        raise ValidationError("Sorry, only .json files are accepted.")
-    except json.decoder.JSONDecodeError:
-        pass
-    except (KeyError, TypeError):
-        raise ValidationError(
-            "Sorry, this file does not contain the right information."
-        )
-    except Exception:
-        raise ValidationError("Sorry, this file isn't valid.")
-
-
 class UploadListeningHistoryForm(forms.Form):
     file_field = forms.FileField(
+        label="Listening History File/s",
         widget=forms.ClearableFileInput(attrs={"multiple": True}),
-        # validators=[validate_json],
     )
