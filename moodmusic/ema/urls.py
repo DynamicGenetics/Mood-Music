3b5f2ab7b43718f90a886cd31f84a9b62d6a30ca
Nina Di Cara <ninadicara@gmail.com>
2020-11-05 16:52:42 +0000

    Make send_ema a management command

--- moodmusic/ema/urls.py
+++ moodmusic/ema/urls.py
@@ -3,5 +3,4 @@ from .views import views
 
 urlpatterns = [
     path("", views.respond_to_incoming_message, name="ema"),
-    path("startsession", views.start_survey_session, name="startsession"),
 ]
--- moodmusic/ema/views/views.py
+++ moodmusic/ema/views/views.py
@@ -1,5 +1,3 @@
-import os
-import random
 import logging
 
 from django.http import HttpResponse
@@ -8,13 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import get_user_model
 
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
-
 from .decorators import validate_twilio_request
 from moodmusic.ema.services.ema_response import manage_response
-from moodmusic.ema.models import EMASession, SessionState, EMAQuestion
 
-# Retieve or create a logger instance
+# Retrieve or create a logger instance
 logger = logging.getLogger(__name__)
 
 
@@ -61,41 +56,8 @@ def respond_to_incoming_message(request):
             )
         )
         reply = "Sorry, we don't know this number. Get in touch if you think that we should."
+
+        # Add the appropriate reply to the message
         resp.message(reply)
-        # Return with a 401 'Unauthorised' code - client must authenticate to get correct response.
+        # Send the reply via Twilio endpoint
         return HttpResponse(str(resp))
-
-
-def start_survey_session(request):
-
-    # Get a random question to start with
-    question = random.choice(EMAQuestion.objects.all())
-
-    # Start an EMA Session that will record an id and start_time
-    session = EMASession.objects.create()
-
-    message = (
-        "Hi, it's time for a survey - please reply within 1 hour. Rate your"
-        + " agreement with the following statements, where 1=not at all and 10=completely:\n"
-        + question.body
-    )
-
-    # Set up the Twilio client to send messages
-    client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
-
-    # Get all the users whose phone numbers are verified.
-    users = get_user_model().objects.filter(phone_verified=True)
-
-    for user in users:
-        # Send message
-
-        client.messages.create(
-            to=user.phone_number.as_e164,
-            from_=os.environ["TWILIO_NUMBER"],
-            body=message,
-        )
-        # Initialise the user's state for this new session
-        s = SessionState.objects.create(user=user, session=session)
-        s.update(question)
-
-    return HttpResponse("Messages successfully sent!", 200)
