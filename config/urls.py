77eca241d55fb7e3227074e82f6aa14f5fd14caa
Nina Di Cara <ninadicara@gmail.com>
2020-11-27 08:38:50 +0000

    Clean up remaining user app links

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("moodmusic.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path("dashboard/", include("moodmusic.dashboard.urls", namespace="dashboard")),
