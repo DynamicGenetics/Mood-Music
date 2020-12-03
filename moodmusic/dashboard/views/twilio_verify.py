83f84c51fff03fce970667fd6216aa9f2ba68439
Nina Di Cara <ninadicara@gmail.com>
2020-12-03 17:33:12 +0000

    Add module docstring and alter verified()

--- moodmusic/dashboard/views/twilio_verify.py
+++ moodmusic/dashboard/views/twilio_verify.py
@@ -1,3 +1,12 @@
+"""Views that control the phone number verification process, using Twilio's Verify API.
+
+These views will request an authentication code from the API for the number the user
+provides, and the use the API again the verify that the user provided code is as excepted.
+
+Further documentation about the Verify API is available here: https://www.twilio.com/docs/verify/api.
+
+The file moodmusic/dashboard/twilio_client.py contains the authentication codes for the API calls."""
+
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
@@ -71,6 +80,9 @@ def token_validation(request):
 @login_required
 def verified(request):
     """View to confirm to user that their number is verified"""
-    if not request.session["is_verified"]:
+    # If the request doesn't contain the expected payload just redirect
+    if "is_verified" in request.session:
+        if request.session["is_verified"]:
+            return render(request, "dashboard/verified.html")
+    else:
         return redirect("dashboard:phone_verification")
-    return render(request, "dashboard/verified.html")
