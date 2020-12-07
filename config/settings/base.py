77e1a5cbaea821a59a046d9a70437daf93788d62
Nina Di Cara <ninadicara@gmail.com>
2020-12-07 12:14:57 +0000

    Update settings so local db connects

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,8 +50,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME", "moodmusic"),
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWD", "postgres"),
+        "USER": os.environ.get("DB_USER", ""),
+        "PASSWORD": os.environ.get("DB_PASSWD", ""),
         "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
         "PORT": os.environ.get("DB_PORT", 5432),
         "CONN_MAX_AGE": 60,
