1928613bd98a291ef33899af0c18acc97cd51c7d
Nina Di Cara <nina.dicara@bristol.ac.uk>
2022-08-04 15:05:15 +0100

    Implement wizard flow instead of central dashboard

--- moodmusic/dashboard/urls.py
+++ moodmusic/dashboard/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("", view=views.dashboard, name="index"),
     path("thanks/", view=views.thanks, name="thanks"),
     path("spotify-connect/", view=views.spotify, name="spotify"),
-    path("mobile-surveys/", view=views.mobile_guide, name="mobile_guide"),
     path("data-drop/", views.datadrop, name="datadrop"),
     path("verification/", views.phone_verification, name="phone_verification"),
     path("verification/token/", views.token_validation, name="token_validation"),
